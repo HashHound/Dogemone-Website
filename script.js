c2e6665a0b11b09de7a440bdc1ed6b98e940d431
HashHound <aravindkishore21@gmail.com>
2024-09-17 23:19:09 +0530

    Revert "Updates to website"
    This reverts commit 05d5cae26a3259b3c60f7dee072d365d05934e07.

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
 document.addEventListener('DOMContentLoaded', function() {
-  console.log('🐕 Woof! Welcome to Dogemone! Time to get meme-ing!');
-  document.querySelectorAll('.btn').forEach(button => {
-    button.addEventListener('click', () => {
-      console.log('You clicked a button! Much wow! 🚀🐕');
-    });
-  });
+  console.log('Welcome to Dogemone!');
 });
