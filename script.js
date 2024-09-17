05d5cae26a3259b3c60f7dee072d365d05934e07
HashHound <aravindkishore21@gmail.com>
2024-09-17 23:09:25 +0530

    Updates to website

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
-  console.log('Welcome to Dogemone!');
+  console.log('🐕 Woof! Welcome to Dogemone! Time to get meme-ing!');
+  document.querySelectorAll('.btn').forEach(button => {
+    button.addEventListener('click', () => {
+      console.log('You clicked a button! Much wow! 🚀🐕');
+    });
+  });
 });
